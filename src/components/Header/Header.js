aca0855a677b356d8173cb30a95c087cfd0408e8
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-03-31 18:53:52 +0600

    firstly nav added then in nav a, Link, CustomLink added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to my fancy Routing Website!</h1>
+            <nav>
+                {/* <a href="/">Home</a>
+                <a href="/friends">Friends</a>
+                <a href="/about">About</a> */}
+
+                {/* <Link to="/">Home</Link>
+                <Link to="/friends">Friends</Link>
+                <Link to="/about">About</Link> */}
+
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/friends">Friends</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+            </nav>
         </div>
     );
 };
